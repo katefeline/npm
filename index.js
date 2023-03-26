dbc68ee259f1f5753793a35fd91528145dd3146a
Kateryna Shadrina <katefeline@gmail.com>
2023-03-26 18:40:34 +0300

    npm, npx

---
+++ index.js
@@ -0,0 +1,15 @@
+const sum = function (a, b) {
+  return a + b;
+};
+
+const subtraction = function (a, b) {
+  return a - b;
+};
+
+const multiply = function (a, b) {
+  return a * b;
+};
+
+console.log(sum(1,2));
+console.log(subtraction(10, 5));
+console.log(multiply(7,3));
